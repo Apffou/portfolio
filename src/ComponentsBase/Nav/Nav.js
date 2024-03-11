6449357be110b53836c9cfd649c0b7b674819eda
Anaïs Zafiriou-Lejeune <anais.ljn@outlook.fr>
2024-03-11 13:57:14 +0100

    Animation menu burger ok

--- src/ComponentsBase/Nav/Nav.js
+++ src/ComponentsBase/Nav/Nav.js
@@ -2,7 +2,8 @@ import { Link } from 'react-router-dom';
 import './Nav.scss';
 function Nav(props) {
     return (
-        <nav className={`menu ${props.isOpen ? 'active' : ''}`}>
+
+        <nav className={`menu ${props.isOpen ? 'active' : ''}`}>   { /* Fonction ternaire pour ajouter la classe activ */}
             <ul className='nav-link'>
                 <Link to="/"><li>Bienvenue</li></Link>
                 <Link to="/ParcoursPro"><li>Parcours Pro</li></Link>
