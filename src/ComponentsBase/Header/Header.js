d76c03d5472b611d47ad8131936f476b94db3213
Anaïs Zafiriou-Lejeune <anais.ljn@outlook.fr>
2024-03-20 18:14:16 +0100

    Au clic en dehors du menu burger le menu se ferme

--- src/ComponentsBase/Header/Header.js
+++ src/ComponentsBase/Header/Header.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Nav from '../Nav/Nav';
 import './Header.scss';
 
@@ -11,12 +11,31 @@ function Header() {
         setMenuOpen(!isMenuOpen)
     }
 
+    //Référence pour le menu pour detecter les clics en dehors du menu
+    const menuRef = useRef(null);
+
+    // Gestion de la fermeture du menu lorsqu'on clique en dehors de celui-ci
+    const handleOutsideClick = (event) => {
+        if (menuRef.current && !menuRef.current.contains(event.target)) {
+            setMenuOpen(false);
+        }
+    };
+
+    // Ecoute des clics en dehors du menu et appel de la fonction handleOutsideClick 
+    useEffect(() => {
+        document.addEventListener('click', handleOutsideClick);
+        return () => {
+            document.removeEventListener('click', handleOutsideClick);
+        };
+        //Tableau vide pour s'assurer que l'effet ne se déclenche qu'une seule fois
+    }, []);
+
     return (
         <header >
             <div className='nav-desktop'>
                 <img src="../assets/images/sunshine.png" alt="sunshine" className='logo' />
                 <div id="burger-button" onClick={toggleMenu}>
-                    <div className={`burger-icon ${isMenuOpen ? 'open' : 'close'}`}>
+                    <div ref={menuRef} className={`burger-icon ${isMenuOpen ? 'open' : 'close'}`}>
                         <span></span>
                         <span></span>
                         <span></span>
