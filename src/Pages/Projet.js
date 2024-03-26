2271a20bcf8ac18fc390f1517cad87c63fe12142
Anaïs Zafiriou-Lejeune <anais.ljn@outlook.fr>
2024-03-26 20:06:18 +0100

    Mise a jour du JSON, Mise a jour du scss Simplecard dans page projet + app

--- src/Pages/Projet.js
+++ src/Pages/Projet.js
@@ -1,4 +1,5 @@
 
+import SimpleCard from '../Components/SimpleCard/SimpleCard';
 import Tags from '../Components/Tags/Tags';
 import projet from '../Projets.json';
 import { useParams } from "react-router-dom";
@@ -19,10 +20,11 @@ function Projet() {
                     <h2>Description</h2>
                     <p>{project.date} </p>
                     <p> {project.description} </p>
-                    <h2>Compétences utilisées</h2>
+                    <h2>Compétences / Outils utilisées</h2>
                     <div className='tags-container'>
-                        {project.tags.map((tags) => <Tags key={tags} nom={tags} />)}
+                        {project.tags.map((tag, index) => <SimpleCard key={index} src={tag.img_src} alt={tag.img_alt} title={tag.title} />)}
                     </div>
+
                     <h2>En savoir plus </h2>
                     <div className='link-projets'>
                         <a href={project.linkGithub} className='icon-projet'>
