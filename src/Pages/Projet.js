78d9cb4c29c29d464f28aef94bedfab436eb0b96
Anaïs Zafiriou-Lejeune <anais.ljn@outlook.fr>
2024-03-26 16:59:44 +0100

    Modification du style CSS sur la page projet

--- src/Pages/Projet.js
+++ src/Pages/Projet.js
@@ -12,31 +12,33 @@ function Projet() {
 
     return (
         <div className='project'>
-            <h1> {project.title} </h1>
+
             <section className='section-description'>
-                <div className='cover-container'>
-                    <img src={project.cover} alt={project.alt} className='cover' />
-                </div>
                 <div className='mission'>
-                    <h2>Description de la mission</h2>
-                    <p>Projet terminé {project.date} </p>
+                    <h1> {project.title} </h1>
+                    <h2>Description</h2>
+                    <p>{project.date} </p>
                     <p> {project.description} </p>
-                    <h3>Compétences utilisés</h3>
+                    <h2>Compétences utilisées</h2>
                     <div className='tags-container'>
                         {project.tags.map((tags) => <Tags key={tags} nom={tags} />)}
                     </div>
-                    <h3>En savoir plus : </h3>
+                    <h2>En savoir plus </h2>
                     <div className='link-projets'>
                         <a href={project.linkGithub} className='icon-projet'>
                             <img src="../../assets/images/github-link.png" alt="icon github" className='icon' />
-                            <span>Voir le projet</span>
+                            <span>Source du projet</span>
                         </a>
                         <a href={project.linkPage} className='icon-projet'>
                             <img src="../../assets/images/page-web.png" alt="icon site web" className='icon' />
-                            <span>Voir le site</span>
+                            <span>Accès au site</span>
                         </a>
                     </div>
                 </div>
+                <div className='cover-container'>
+                    <img src={project.cover} alt={project.alt} className='cover' />
+                </div>
+
             </section>
         </div>
     )
