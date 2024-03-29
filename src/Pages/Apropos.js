25d267072b32450755fdb71c9a217ea772c19483
Anaïs Zafiriou-Lejeune <anais.ljn@outlook.fr>
2024-03-29 16:08:46 +0100

    MaJ du JSON et Contenu terminé pour la page A propos

--- src/Pages/Apropos.js
+++ src/Pages/Apropos.js
@@ -1,12 +1,84 @@
 
 function Apropos() {
     return (
-        <div>
-            <h1>Quelques chiffres clés (en mode infographie et des gros chiffres) </h1>
-            <h2>Mes objectifs de vie </h2>
-            <h2>Mes Hobbies (Photographie) (Jeux videos) (Doublage Français) (Cinéma/Séries) (Lire) </h2>
+        <>
+            <section className="histoire">
+                <h1>À propos </h1>
+                <p>Pour la petite histoire en 2016, j'ai entrepris une reconversion professionnelle pour devenir infographiste Print,
+                    passionné par la mise en page. Au cours de cette période, j'ai exploré les bases du web, une expérience qui m'a captivé.
+                </p>
+                <p>Quelques années plus tard,  j'ai ressenti le désir d'évoluer et d'approfondir mes connaissances en matière de conception web.
+                    Après avoir suivi une formation en Web design, qui m'a permis d'apprendre les bonnes pratiques à suivre et surtout celles
+                    à éviter, j'ai ressenti le besoin d'aller plus loin. Mon ambition était de pouvoir concrétiser mes maquettes en code.
+                </p>
+                <p>C'est ainsi que j'ai entrepris ce nouveau projet pour devenir Intégratrice / Développeuse Front-end.</p>
+            </section>
+            <section className="objectifs">
+                <h2>Mes objectifs</h2>
+                <div>
+                    <h3>Eco Conception</h3>
+                    <p>Apprendre et intégrer des pratiques d'éco-conception dans mes projets web afin de réduire l'empreinte écologique des applications et promouvoir
+                        une approche durable du numérique.
+                    </p>
+                </div>
+                <div>
+                    <h3>UX Design</h3>
+                    <p>Approfondir ma compréhension des principes fondamentaux de l'UX design en suivant des cours en ligne, en lisant des livres spécialisés et en pratiquant des exercices de conception.</p>
+                </div>
+                <div>
+                    <h3>Gestion du stress</h3>
+                    <p>Améliorer ma capacité à gérer le stress et à développer des stratégies efficaces pour maintenir un état d'esprit positif et résilient
+                        dans les situations difficiles.</p>
+                </div>
+            </section>
+            <section className="hobbies">
+                <h2>Mes Hobbies</h2>
+                <div>
+                    <img src="" alt="" />
+                    <h2>Photographie </h2>
+                    <p>"Je pratique depuis peu la photographie animalière et quel plaisir de réussir à capturer un cliché.
+                        Le monde qui nous entoure est rempli de beauté si l'on prends patience pour le capter. Savourer juste le bruit de la nature et ouvrir grand les
+                        yeux sont mes petits plaisirs de la vie. "</p>
+                </div>
 
-        </div>
+                <div>
+                    <img src="" alt="" />
+                    <h2>Lecture</h2>
+                    <p>" J'adore les livres. Toute petite on m'a lu des contes pour m'endormir, puis je les ai lus dès que je pouvais, partout, tout le temps.
+                        Chaque livre est une invitation à l'aventure, une opportunité d'apprendre,de réver et de réflechir. L'odeur du papier et de l'encre ♥"</p>
+                </div>
+
+                <div>
+                    <img src="" alt="" />
+                    <h2>Jeux Vidéos</h2>
+                    <p>"Geek ? Oui totalement. Les jeux vidéos sont des belles histoires ou l'on nous permet de jouer un rôle. Je suis fascinée
+                        par la créativité sans fin du monde des jeux vidéos que ce soit dans le gameplay, dans la patte graphique ou les musiques. "</p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <h2>Cinéma/Séries : ""</h2>
+                    <p></p>
+                </div>
+
+                <div>
+                    <img src="" alt="" />
+                    <h2>Doublage Français  </h2>
+                    <p>
+                        "Team VO ou VF ? Aucun, c'est un faux débat. J'admire beaucoup nos acteurs et actrices qui travaillent dans le doublage
+                        tel que Richard Darbois (Buzz L'éclair, Harrison Ford) ou Barbara Beretta (Laguertha dans Vikings, Reine Maeve dans The Boys).   "
+                    </p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <h2>Les chiens : "" </h2>
+                    <p>
+
+                    </p>
+                </div>
+
+            </section>
+
+        </>
     )
 }
 export default Apropos;
